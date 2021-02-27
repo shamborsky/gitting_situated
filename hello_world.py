6aa9aa843f8dfe4d83ce2941bdfab2f31b790636
shamborsky <seanhamborsky@gmail.com>
2021-02-27 09:23:07 -0500

    Updated calculator functionality

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,22 @@
 print("Hello World Boye")
 
-print("Testing testin 1 2 3")
+num1 = input("enter first number: ")
+num2 = input("enter second number: ")
+operator = input("whatcha wanna do? \nAdd\nSubtract\nMultiply\nDivide\n\n")
+
+if operator == "Add":
+    suem = int(num1) + int(num2)
+    print("Your total is " + str(suem))
+elif operator == "Subtract":
+    suem = int(num1) - int(num2)
+    print("Your total is " + str(suem))
+elif operator == "Multiply":
+    suem = int(num1) * int(num2)
+    print("Your total is " + str(suem))
+elif operator == "Divide":
+    suem = int(num1) / int(num2)
+    print("Your total is " + str(suem))
+else:
+    print("Please pick from one of the options")
+
+print("\nHave a nice day!")
